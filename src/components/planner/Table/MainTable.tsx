c52afd88eb4f34741eaad15e39274883bc353ec0
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-08-23 13:44:36 +0200

    swipe added to table

--- src/components/planner/Table/MainTable.tsx
+++ src/components/planner/Table/MainTable.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import { Table } from '@mantine/core';
 import { applyFilters } from '@/utils/filterHelpers';
@@ -8,18 +8,34 @@ import { useFilterContext } from '@/providers/FilterTableProvider';
 import { TablePagination } from './TablePagination';
 import { useRiderContext } from '@/providers/RiderProvider';
 import { SortIcon } from './SortIcon';
+import { useSwipe } from '@/utils/swipeUtils';
 
 
 export function MainTable() {
   const { globalRiders } = useRiderContext();
-  const { filters, isReset, page  } = useFilterContext();
+  const { filters, isReset, page, setPage  } = useFilterContext();
   const [data, setData] = useState(chunk(sortData(globalRiders || [], 'price', false)));
   const [isSortedDesc, setIsSortedDesc] = useState(false);
   const [sortType, setSortType] = useState('price');
+  const containerRef = useRef<HTMLTableElement>(null);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchStartY, setTouchStartY] = useState(0);
 
   const updateData = () => {
     setData(chunk(sortData(applyFilters(globalRiders || [], filters), sortType, isSortedDesc)));
   }
+
+  const onSwipe = (direction: 'left' | 'right') => {
+    if (direction === 'left') {
+      if(page >= data.length) return;
+      setPage(page + 1);
+    } else {
+      if(page <= 1) return;
+      setPage(page - 1);
+    }
+  }
+
+  useSwipe({ ref: containerRef, onSwipe, touchStartX, touchStartY, setTouchStartX, setTouchStartY });
 
   useEffect(() => {
     updateData();
@@ -50,7 +66,14 @@ export function MainTable() {
   
   return (
     <div className={classes.container}>
-      <Table miw={100} highlightOnHover horizontalSpacing={'xs'} className={classes.table} layout='fixed'>
+      <Table 
+      miw={100} 
+      highlightOnHover 
+      horizontalSpacing={'xs'} 
+      className={classes.table} 
+      layout='fixed'
+      ref={containerRef}
+      >
         <Table.Thead>
           <Table.Tr style={{borderBottom: '1px solid var(--highlight-grey)'}}>
             <Table.Th className={classes.column1}></Table.Th>
