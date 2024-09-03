10811ff5a940957c4f822d7f9c05a44a66093db6
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-09-03 20:28:16 +0200

    icons added to notification

--- src/components/planner/Map/UpdateNotification.tsx
+++ src/components/planner/Map/UpdateNotification.tsx
@@ -1,5 +1,6 @@
 import { Notification } from '@mantine/core';
 import classes from '@/styles/Map/Notification.module.css';
+import { IconCheck, IconX } from '@tabler/icons-react';
 
 
 export function UpdateNotification() {
@@ -8,6 +9,7 @@ export function UpdateNotification() {
             color="green"
             title="Planen ble oppdatert"
             withBorder
+            icon={<IconCheck />}
             classNames={classes}
             withCloseButton={false}
         >
@@ -22,6 +24,7 @@ export function UpdateNotificationFail() {
             title="Planen ble ikke oppdatert"
             withBorder
             classNames={classes}
+            icon={<IconX />}
             withCloseButton={false}
         >
             Noe gikk galt
