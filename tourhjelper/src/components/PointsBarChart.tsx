626dbf2facb9fbd253ca0440bca9140e7621c67d
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-07-04 16:53:06 +0200

    fix: update stage points data and enhance PointsBarChart responsiveness

--- tourhjelper/src/components/PointsBarChart.tsx
+++ tourhjelper/src/components/PointsBarChart.tsx
@@ -4,6 +4,15 @@ import styles from "@/styles/BarChart/PointsBarChart.module.css";
 
 const PointsBarChart = () => {
   const [data, setData] = useState<any[]>([]);
+  const [deviceWidth, setDeviceWidth] = useState(0);
+
+  const getBarColor = (value: number) => {
+    if (value >= 300) return "cyan.5";
+    if (value >= 250) return "green.5";
+    if (value >= 180) return "yellow.6";
+    if (value >= 150) return "orange.7";
+    return "red.7";
+  };
 
   const fetchData = async () => {
     try {
@@ -23,6 +32,17 @@ const PointsBarChart = () => {
     fetchData().then(setData);
   }, []);
 
+  useEffect(() => {
+    setDeviceWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setDeviceWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles.barChartContainer}>
       <div className={styles.header}>
@@ -31,13 +51,16 @@ const PointsBarChart = () => {
       <BarChart
         data={data}
         dataKey="etappe"
+        getBarColor={(value) => getBarColor(value)}
         series={[{ name: "Poeng", color: "red.7" }]}
-        h={400}
+        h={deviceWidth < 600 ? 800 : 400}
         barProps={{ radius: 20 }}
         gridAxis="none"
-        unit="p."
-        maxBarWidth={22}
+        withYAxis={deviceWidth < 600 ? true : false}
+        unit={deviceWidth < 600 ? "" : "p."}
+        maxBarWidth={deviceWidth < 800 ? 12 : 22}
         className={styles.barChart}
+        orientation={deviceWidth < 600 ? "vertical" : "horizontal"}
       />
     </div>
   );
