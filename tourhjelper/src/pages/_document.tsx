9c8ca0be7c2c19cf0eeff3cc5bd594d70b9812bc
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-07-04 17:53:40 +0200

    fix: add title to the document head for improved SEO

--- tourhjelper/src/pages/_document.tsx
+++ tourhjelper/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>Tourhjelper</title>
         <ColorSchemeScript defaultColorScheme="auto" />
         <link rel="icon" href="/favicon_io/favicon.ico" />
         <link
